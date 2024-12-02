4006da88d184285160819f52dae478e53691fc45
sehoon.gim <sehoongim@gmail.com>
2024-12-02 16:19:16 +0900

    Initial Commit

---
+++ EventViewer/EventViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventViewerApp.swift
+//  EventViewer
+//
+//  Created by sehoon gim on 12/2/24.
+//
+
+import SwiftUI
+
+@main
+struct EventViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
